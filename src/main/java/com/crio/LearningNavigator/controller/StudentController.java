4ad42f6d6843ef2300bff2b76379a46a0e771a69
pgprajwal <pgprajwal99@gmail.com>
2024-04-25 13:26:26 +0530

    Added few more method signatures in StudentController

--- src/main/java/com/crio/LearningNavigator/controller/StudentController.java
+++ src/main/java/com/crio/LearningNavigator/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,16 @@ public class StudentController {
 
     @DeleteMapping("/{studentId}")
     public ResponseEntity<String> deregisterStudent(@PathVariable long studentId) {
+        return null;
+    }
+
+    @PutMapping("/{studentId}/subject/{subjectId}")
+    public ResponseEntity<Student> enrollStudentInSubject(@PathVariable long studentId, @PathVariable long subjectId) {
+        return null;
+    }
+
+    @PutMapping("/{studentId}/exam/{examId}")
+    public ResponseEntity<Student> registerStudentForExam(@PathVariable long studentId, @PathVariable long examId) {
         return null;
     }
 }
